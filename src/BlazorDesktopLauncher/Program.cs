56e2829ac371340f606fafeb224ad1a853ff151e
Y56380X <Y56380X@users.noreply.github.com>
2020-08-27 19:07:15 +0200

    start Puppeteer browser

--- src/BlazorDesktopLauncher/Program.cs
+++ src/BlazorDesktopLauncher/Program.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
 
 namespace BlazorDesktopLauncher
 {
@@ -19,7 +21,30 @@ namespace BlazorDesktopLauncher
         public static void Start(string[] args, Type mainPage)
         {
             MainPage = mainPage;
-            CreateHostBuilder(args, mainPage).Build().Run();
+            var runTask = CreateHostBuilder(args, mainPage).Build().RunAsync();
+            OpenWindow();
+            runTask.Wait();
+        }
+
+        private static Task OpenWindow()
+        {
+            return Task.Run(async () =>
+            {
+                await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+				
+                var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+                {
+                    Headless = false,
+                    Args = new []
+                    {
+                        "--app=http://localhost:5005/", 
+                        "--window-size=900,650", 
+                        "--allow-insecure-localhost",
+                        "--disable-extensions"
+                    }
+                });
+                browser.Closed += (sender, eventArgs) => Environment.Exit(0);
+            });
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args, Type mainPage) =>
@@ -27,11 +52,12 @@ namespace BlazorDesktopLauncher
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStaticWebAssets();
-                    webBuilder.UseWebRoot(GetWebRootPath(mainPage));
+                    webBuilder.UseWebRoot(GetWebRootPath());
+                    webBuilder.UseUrls("http://localhost:5005/");
                     webBuilder.UseStartup<Startup>();
                 });
 
-        private static string GetWebRootPath(Type type) => 
-            Path.Combine(Path.GetDirectoryName(type.Assembly.Location)!, "wwwroot");
+        private static string GetWebRootPath() => 
+            Path.Combine(Environment.CurrentDirectory, "wwwroot");
     }
 }
