50aa4607b088aed9e36016e44a073a0b21804c1f
Y56380X <Y56380X@users.noreply.github.com>
2020-08-28 13:19:51 +0200

    js interop example

--- samples/SimpleApp/ExampleJsInterop.cs
+++ samples/SimpleApp/ExampleJsInterop.cs
@@ -5,22 +5,18 @@ namespace SimpleApp
 {
     public class ExampleJsInterop
     {
-        private readonly IJSRuntime _jsRuntime;
-
-        public ExampleJsInterop(IJSRuntime jsRuntime) => _jsRuntime = jsRuntime;
-
-        public ValueTask<string> Prompt(string message)
+        public ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
         {
             // Implemented in exampleJsInterop.js
-            return _jsRuntime.InvokeAsync<string>(
+            return jsRuntime.InvokeAsync<string>(
                 "exampleJsFunctions.showPrompt",
                 message);
         }
         
-        public async Task Alert(string message)
+        public async Task Alert(IJSRuntime jsRuntime, string message)
         {
             // Implemented in exampleJsInterop.js
-            await _jsRuntime.InvokeVoidAsync(
+            await jsRuntime.InvokeVoidAsync(
                 "exampleJsFunctions.showAlert", message);
         }
     }
